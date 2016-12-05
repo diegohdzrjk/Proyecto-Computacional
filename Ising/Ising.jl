d0e6e8215d0864598a9c2b873002658ed2ea393e
Diego Hernandez <dhernandezrjk@ciencias.unam.mx>
2016-12-05 09:27:00 -0600

    Se mete las simulaciones de la noche

--- Ising/Ising.jl
+++ Ising/Ising.jl
@@ -7,12 +7,11 @@ using Ising_cuad
 #simulacion(0.5,20,1.0,3.5,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
 #simulacion(0.5,50,1.0,3.5,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
 
-simulacion(0.5,100,1.0,3.5,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
-simulacion(0.5,150,1.36,1.65,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
-simulacion(0.5,150,1.99,2.28,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
-simulacion(0.5,150,2.63,2.92,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
-simulacion(0.5,150,2.63,2.92,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
+#simulacion(0.5,100,1.0,3.5,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
+#simulacion(0.5,150,1.36,1.65,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
+#simulacion(0.5,150,1.99,2.28,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
 
+simulacion(0.5,150,2.63,2.92,0.01,[50000, 5000], "/Users/diego/Documents/Proyecto-Computacional/Ising")
 
 #simulacion_images(0.5,150,0.5,4.5,2.0,[20000, 500], "/Users/diego/Documents/Proyecto-Computacional/Ising")
 #simulacion_images(0.5,150,2.1,2.3,0.1,[50000, 500], "/Users/diego/Documents/Proyecto-Computacional/Ising")
